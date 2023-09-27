b272845323b70c617c17387c23101e91a48bc17a
John Baldonado <john.baldonado@simplerent.com.au>
2023-09-27 09:24:14 +0800

    chore: add docs deployment workflow

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Vue UI',
   description: 'Exquisite UI built using Tailwind and Zag',
+  base: '/vue-ui',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
