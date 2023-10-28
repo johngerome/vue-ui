41d0018735603d96002fc634e20a014a55c1ca00
johngerome <johngerome@gmail.com>
2023-10-28 11:52:55 +0800

    fix(accordion): add theme configuration

--- src/components/Accordion/AccordionContent.vue
+++ src/components/Accordion/AccordionContent.vue
@@ -1,5 +1,19 @@
+<script lang="ts" setup>
+import { cn } from '@/util/cn'
+import { useAttrs } from 'vue'
+
+const attrs = useAttrs()
+</script>
+
 <template>
-  <div class="overflow-hidden transition-all duration-300 ease-in-out py-4">
+  <div
+    :class="
+      cn(
+        'overflow-hidden transition-all duration-300 ease-in-out py-4',
+        attrs?.class || '',
+      )
+    "
+  >
     <slot />
   </div>
 </template>
